fceeb9426dd93b97f9a3334c97f515c9c75c5223
anabranch <wac.chambers@gmail.com>
2015-04-26 14:36:02 -0700

    Initial Bower Level Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global SFCViewer, $*/
+
+
+window.SFCViewer = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    SFCViewer.init();
+});
