74c961e2c79d1726c9f8c771e0ea5d88af3f4ac2
semerdj <jsemer@gmail.com>
2015-05-02 17:08:45 -0700

    add test geojson and leaflet-draw

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,11 +14,24 @@ window.SFCViewer = {
         withCredentials: true
       }
     }
-    window.Map = L.map('map').setView([37.76, -122.44], 12);
+    window.Map = L.map('map').setView([37.77, -122.44], 13);
+    
     L.tileLayer('http://{s}.tiles.mapbox.com/v3/billc.lj7dn4cg/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
     }).addTo(window.Map);
+
+// Initialise the FeatureGroup to store editable layers
+var drawnItems = new L.FeatureGroup();
+window.Map.addLayer(drawnItems);
+
+// Initialise the draw control and pass it the FeatureGroup of editable layers
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+window.Map.addControl(drawControl);
 
   }
 };
@@ -26,39 +39,4 @@ window.SFCViewer = {
 $(document).ready(function() {
   'use strict';
   SFCViewer.init();
-  var hardly = new SFCViewer.Models.SfEvent({
-    "id": "hardly-strictly-2014"
-  });
-  var temp = new SFCViewer.Models.Polygon({
-    "coordinates": [
-      [
-        37.76128348360843, -122.42841124534607
-      ],
-      [
-        37.7580942260561, -122.42810010910034
-      ],
-      [
-        37.75822145970878, -122.42584705352783
-      ],
-      [
-        37.76141071177564, -122.42613673210143
-      ],
-      [
-        37.76128348360843, -122.42841124534607
-      ]
-    ],
-    "data": {}
-  });
-
-  hardly.fetch(window.XHRHelper);
-  temp.fetch(window.XHRHelper);
-  var temp_render = new SFCViewer.Views.MapDisplay({
-    model: temp
-  });
-  temp_render.render();
-  var new_render = new SFCViewer.Views.MapDisplay({
-    model: hardly
-  });
-  console.log(temp_render);
-  new_render.render();
 });
