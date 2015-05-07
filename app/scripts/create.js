f66945ffb9d489181ea24c38c9c21e583df79f08
anabranch <wac.chambers@gmail.com>
2015-05-06 20:31:32 -0700

    Basic form is setup, still no post action, no info from map

--- app/scripts/create.js
+++ app/scripts/create.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
 
     //Date Picker
-    $('#datepicker').datepicker();
+    $('#datePicker').datepicker();
 
-    //Mapping Information
+    // Mapping Setup
     var mapbox_pk = "pk.eyJ1IjoiYmlsbGMiLCJhIjoiYllENmI2VSJ9.7 wxYGAIJoOtQ2WE3zoCJEA";
-    window.Map = L.map('map').setView([37.77, -122.44], 13);
+    window.Map = L.map('map').setView([37.77, -122.44], 12);
 
     L.tileLayer('http://{s}.tiles.mapbox.com/v3/billc.lj7dn4cg/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     }).addTo(window.Map);
 
-    // Initialise the FeatureGroup to store editable layers
-    var drawnItems = new L.FeatureGroup();
+    var drawnItems = new L.FeatureGroup(); // the items that will be drawn
     window.Map.addLayer(drawnItems);
 
-    // Initialise the draw control and pass it the FeatureGroup of editable layers
     var drawControl = new L.Control.Draw({
-        edit: {
+        draw: { // controls what features show up
+            polyline: false,
+            marker: false
+        },
+        edit: { // adds the feature groups
             featureGroup: drawnItems,
         }
     });
@@ -26,15 +28,28 @@ $(document).ready(function() {
     window.Map.on('draw:created', function(e) {
         var type = e.layerType,
             layer = e.layer;
+        drawnItems.addLayer(layer);
 
         // Need to setup drawing
         if (layer.toGeoJSON().geometry.type === "Polygon") {
 
         }
-
-        drawnItems.addLayer(layer);
-
     });
-    // // End Mapping Information
+    // End Mapping Setup
 
+    // Create Form submit
+    function createFormSubmit(){
+        var startDate1 = $('#startDate1').val(),
+            endDate1 = $('#endDate1').val(),
+            startDate2 = $('#startDate2').val(),
+            endDate2 = $('#endDate2').val(),
+            eventName = $('#eventName').val();
+
+        console.log(startDate);
+        console.log(endDate);
+        console.log(eventName);
+        return false; // prevent refresh of page
+    }
+
+    $("#creationForm").submit(createFormSubmit);
 });
