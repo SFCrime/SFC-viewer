9b6a47d6f81e6ef5933e2a4b4a193b371316d395
anabranch <wac.chambers@gmail.com>
2015-05-07 18:54:17 -0700

    Working on abstracting out common function + build process

--- app/scripts/graphs.js
+++ app/scripts/graphs.js
@@ -5,62 +5,35 @@ $(document).ready(function() {
     var renderData = function(data) {
         var response = JSON.parse(data.response);
         console.log(response);
-        drawMarkerArea(response.geojson_crime[0]);
+        renderMarkerArea(response.geojson_crime[0]);
         //drawMarkerArea(response.geojson_crime[1].features);
     }
 
-    var RedIcon = L.Icon.Default.extend({
-        options: {
-            iconUrl: 'images/marker-icon-red.png'
+    var renderRowChart = function (divId, groupname, dimension, group){
+        var height = 300, width = 450;
+        if (group.size() < 4){
+            height = 200;
         }
-    });
-    var redIcon = new RedIcon();
-
-
-    if (!((setUp.getParams().type == "") || (setUp.getParams().type === undefined))) {
-        url = setUp.api_url_base + $.param(setUp.getParams()); // do we have query params? if so set the url
-    } else { // give a default view...likely change this in the future
-        url = "http://localhost:5000/api/v1/crime/?type=1v1&geo_type=Polygon&coordinates=-122.43114709854127+37.75737492779443%2C-122.43114709854127+37.76243022568955%2C-122.42318630218506+37.76243022568955%2C-122.42318630218506+37.75737492779443%2C-122.43114709854127+37.75737492779443&start_date=09-07-2014&end_date=09-20-2014"
+        return dc.rowChart(divId, groupname)
+            .dimension(dimension)
+            .group(group)
+            .height(height);
     }
 
-    d3.xhr(url, renderData);
+
+    var renderBarChart = function (divId, groupname, dimension, group){
+        var height = 200, width = 450;
+        return dc.barChart(divId, groupname)
+            .dimension(dimension)
+            .group(group)
+            .height(height);
+    }
 
 
-    function drawMarkerArea(inputGeoJSON) { // must accept only a geojson object
-        var data = crossfilter(inputGeoJSON.features),
-            groupname = "marker-area",
-            crime = data.dimension(function(d) {
-                return d.geometry.coordinates[1] + "," + d.geometry.coordinates[0];
-            }),
-            crimeGroup = crime.group(),
-            category = data.dimension(function(d) {
-                return d.properties.category;
-            }),
-            categoryGroup = category.group().reduceCount(),
-            dayofweek = data.dimension(function(d) {
-                var day = new Date(d.properties.date).getDay();
-                var name = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satday'];
-                return day + '.' + name[day];
-            }),
-            dayofweekGroup = dayofweek.group().reduceCount(),
-            crimesbyday = data.dimension(function(d) {
-                return new Date(d.properties.date);
-            }),
-            crimesbydayGroup = crimesbyday.group().reduceCount();
-
-        window.data = data;
-
-        // find earliest and latest dates and add 1 hour buffer
-        var topDate = crimesbyday.top(1),
-            maxTmp = new Date(topDate[0].properties.date),
-            maxDate = new Date(maxTmp.setHours(maxTmp.getHours() + 1)),
-            bottomDate = crimesbyday.bottom(1),
-            minTmp = new Date(bottomDate[0].properties.date),
-            minDate = new Date(minTmp.setHours(minTmp.getHours() - 1));
-
-        dc.leafletMarkerChart("#map", groupname)
-            .dimension(crime)
-            .group(crimeGroup)
+    var renderLeafletChart = function (divId, groupname, dimension, group){
+     return dc.leafletMarkerChart(divId, groupname)
+            .dimension(dimension)
+            .group(group)
             .width(500)
             .height(500)
             .marker(function(d, map) {
@@ -69,7 +42,7 @@ $(document).ready(function() {
                 var marker = new L.Marker(d.key.split(","), {
                     title: "fuck off",
                     riseOnHover: true,
-                    //         icon: redIcon
+                             icon: redIcon
                 });
                 return marker;
             })
@@ -91,34 +64,77 @@ $(document).ready(function() {
             .zoom(12)
             .renderPopup(true)
             .filterByArea(true);
+    }
+    
+    
+    var RedIcon = L.Icon.Default.extend({
+        options: {
+            iconUrl: 'images/marker-icon-red.png'
+        }
+    });
+    var redIcon = new RedIcon();
 
-        dc.rowChart("#category", groupname)
-            .dimension(category)
-            .group(categoryGroup)
-            .height(300)
-            .width(450)
+
+    if (!((setUp.getParams().type == "") || (setUp.getParams().type === undefined))) {
+        url = setUp.api_url_base + $.param(setUp.getParams()); // do we have query params? if so set the url
+    } else { // give a default view...likely change this in the future
+        url = "http://localhost:5000/api/v1/crime/?type=1v1&geo_type=Polygon&coordinates=-122.43114709854127+37.75737492779443%2C-122.43114709854127+37.76243022568955%2C-122.42318630218506+37.76243022568955%2C-122.42318630218506+37.75737492779443%2C-122.43114709854127+37.75737492779443&start_date=09-07-2014&end_date=09-20-2014"
+    }
+
+    d3.xhr(url, renderData);
+
+
+    
+    function renderMarkerArea(inputGeoJSON) { // must accept only a geojson object
+        window.inputGeoJSON = inputGeoJSON;
+        var data = crossfilter(inputGeoJSON.features),
+            groupname = "marker-area",
+            crime = data.dimension(function(d) {
+                return d.geometry.coordinates[1] + "," + d.geometry.coordinates[0];
+            }),
+            crimeGroup = crime.group(),
+            category = data.dimension(function(d) {
+                return d.properties.category;
+            }),
+            categoryGroup = category.group().reduceCount(),
+            dayofweek = data.dimension(function(d) {
+                var day = new Date(d.properties.date).getDay();
+                var name = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satday'];
+                return day + '.' + name[day];
+            }),
+            dayofweekGroup = dayofweek.group().reduceCount(),
+            crimesbyday = data.dimension(function(d) {
+                return new Date(d.properties.date);
+            }),
+            crimesbydayGroup = crimesbyday.group().reduceCount();
+
+        renderLeafletChart("#map", groupname, crime, crimeGroup);
+
+        renderRowChart("#category", groupname, category, categoryGroup)
             .elasticX(true)
             .colors(["#2ca25f"])
             .xAxis().ticks(2).tickFormat(d3.format("s"));
-
-        dc.rowChart("#dayofweek", groupname)
-            .dimension(dayofweek)
-            .group(dayofweekGroup)
-            .height(150)
-            .width(450)
+        
+        renderRowChart("#dayofweek", groupname, dayofweek, dayofweekGroup)
             .elasticX(true)
-            .colors(["#2b8cbe"])
+           .colors(["#2b8cbe"])
             .label(function(d) {
-                return d.key.split('.')[1];
-            })
+                 return d.key.split('.')[1];
+             })
             .xAxis().ticks(2).tickFormat(d3.format("s"));
 
-        dc.barChart("#crimesbyday", groupname)
-            .dimension(crimesbyday)
-            .group(crimesbydayGroup)
-            .width(450)
-            .height(200)
-            .transitionDuration(500)
+
+
+        
+        // find earliest and latest dates and add 1 hour buffer
+        var topDate = crimesbyday.top(1),
+            maxTmp = new Date(topDate[0].properties.date),
+            maxDate = new Date(maxTmp.setHours(maxTmp.getHours() + 1)),
+            bottomDate = crimesbyday.bottom(1),
+            minTmp = new Date(bottomDate[0].properties.date),
+            minDate = new Date(minTmp.setHours(minTmp.getHours() - 1));
+        
+        renderBarChart("#crimesbyday", groupname, crimesbyday, crimesbydayGroup)
             .elasticY(true)
             .x(d3.time.scale().domain([minDate, maxDate]))
             .yAxis().ticks(2).tickFormat(d3.format("s"));
