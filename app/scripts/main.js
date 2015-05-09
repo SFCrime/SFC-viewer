2b1729387b2de6b6e2751566bacbc50d998cec4f
anabranch <wac.chambers@gmail.com>
2015-05-09 12:06:32 -0700

    Added in police districts
    @semerj at this point things are getting hairy, I'm not sure what we
    should be completely doing but we should really focus on telling a
    story. I'd like to try and pick one and go for it. I think that we
    either tell a alcohol related story or we try and do some gentrification
    of the mission district.
    
    The page format is good for developing on.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,135 +5,72 @@ $(document).ready(function() {
 
     var mapbox_pk = "pk.eyJ1IjoiYmlsbGMiLCJhIjoiYllENmI2VSJ9.7 wxYGAIJoOtQ2WE3zoCJEA";
     var urlBase = "http://localhost:5000/api/v1/crime/?";
+    var map = L.map("story1map").setView([37.77, -122.44], 13);
+    L.tileLayer('http://{s}.tiles.mapbox.com/v3/billc.lj7dn4cg/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    }).addTo(map);
 
 
-    var story = function(divId) {
-        var map = L.map(divId + "map").setView([37.77, -122.44], 13);
-        L.tileLayer('http://{s}.tiles.mapbox.com/v3/billc.lj7dn4cg/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        }).addTo(map);
-
+    var story = function(divId, divs) {
         var selector;
-        for (var divclass in this.divs) {
+        for (var divclass in divs) {
             selector = "#" + divId + " > ." + divclass; // as long as it's in the map story part
-            $(selector).text(this.divs[divclass]);
+            $(selector).text(divs[divclass]);
         }
 
-        var feats = this.geoJSON.features;
-        var chunk = this.geoJSON.features.length / 24;
-
-        for (var i = 0; i < 5; i++) {
-            setTimeout(this.renderFunc, i * 1000, map, {
-                type: "FeatureCollection",
-                features: feats.slice(i * chunk, (i + 1) * chunk)
-            });
-        }
-        // d3
-        var groups = _.groupBy(feats, function(d) {
-            return d.properties.date
-        });
-
-        var parseDate = d3.time.format("%Y-%m-%d").parse;
-
-        var data = [];
-        for (var f in groups) {
-            data.push({
-                date: parseDate(f),
-                details: groups[f].length
-            });
-        }
-        var margin = {
-                top: 2,
-                right: 2,
-                bottom: 3,
-                left: 5
-            },
-            width = 400 - margin.left - margin.right,
-            height = 200 - margin.top - margin.bottom;
-
-
-        var x = d3.time.scale()
-            .range([0, width]);
-
-        var y = d3.scale.linear()
-            .range([height, 0]);
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
-
-        var line = d3.svg.line()
-            .x(function(d) {
-                return x(d.date);
-            })
-            .y(function(d) {
-                return y(d.details);
-            });
-
-        var svg = d3.select("#"+divId + "-d3").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        x.domain(d3.extent(data, function(d) {
-            return d.date;
-        }));
-        y.domain(d3.extent(data, function(d) {
-            return d.details;
-        }));
-
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Total Crime");
-        
-        svg.append("path")
-            .datum(data)
-            .attr("class", "line")
-            .attr("d", line);
-        
-
-
-
-        //end d3
-
-        return {
-            map: map
-        }
 
     }
 
+    $.getJSON("scripts/districts.json", function(data){
+        L.geoJson(data, {
+            "style": {
+                "color": "#004070",
+                "weight": 4,
+                "opacity": 1
+            }
+        }).addTo(map);
+    });
+    
+    
     // Story 1
     d3.xhr(urlBase + $.param({
-        start_date: "10-30-2014",
+        start_date: "10-29-2014",
         end_date: "11-3-2014"
     }), function(resp) {
-        story.call({
-            geoJSON: JSON.parse(resp.response).geojson_crime,
-            divs: {
-                title: "First Story Title",
-                descrip: "This is the description, someone did something. We can see that we have a rise in something over the time of this. Click the button below to animate the story and see the next thing that comes along."
-            },
-            renderFunc: function(map, geoJSON) {
-                L.geoJson(geoJSON).addTo(map);
-            }
+        var divId = "story1";
 
-        }, "story1");
+        // set divs
+        story(divId, {
+            title: "Halloween 2014",
+            descrip: "This is the description, someone did something. We can see that we have a rise in something over the time of this. Click the button below to animate the story and see the next thing that comes along."
+        })
+
+
+        //mapping
+        var geoJSON = JSON.parse(resp.response).geojson_crime;
+        //L.geoJson(geoJSON).addTo(map);
+        // end mapping
+
+        // dimple
+        var groups = _.groupBy(geoJSON.features, function(d) {
+            return d.properties.date;
+        });
+        var data = [];
+        for (var f in groups) {
+            data.push({
+                date: f,
+                category: "All",
+                "Total Crimes": groups[f].length
+            });
+        }
+        var svg = dimple.newSvg("#" + divId + "-d3", 500, 200);
+        var myChart = new dimple.chart(svg, data);
+        myChart.addTimeAxis("x", "date", "%Y-%m-%d", "%m-%d");
+        myChart.addMeasureAxis("y", "Total Crimes");
+        myChart.addSeries("category", dimple.plot.line);
+        myChart.draw();
+        //end dimple
+
     });
 
 
--- app/scripts/setup.js
+++ app/scripts/setup.js
@@ -7,10 +7,15 @@ window.setupApp = function() {
     };
     var api_url_base = "http://localhost:5000/api/v1/crime/?";
 
-    // Make sure window size is nice...
-    $('#map').height($(window).height()-70);
-    // End window resize
-    
+    var sizing = function() {
+        // Make sure window size is nice...
+        $('#map').height($(window).height() - 70);
+        $("#story1-d3").width($("#story1")
+                              .width());
+        // End window resize
+    }
+
+    sizing();
     return {
         api_url_base: api_url_base,
     }
